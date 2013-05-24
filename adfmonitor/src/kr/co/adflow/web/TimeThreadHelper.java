b295a72eaf8379948dbc29fd16ec75e465af0bbd
sm_yang <smyang@adflow.co.kr>
2013-05-24 14:29:32 +0900

    i add a method to TimeThreadHelper, getStackTrace.

--- adfmonitor/src/kr/co/adflow/web/TimeThreadHelper.java
+++ adfmonitor/src/kr/co/adflow/web/TimeThreadHelper.java
@@ -1,5 +1,8 @@
 package kr.co.adflow.web;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadInfo;
+import java.lang.management.ThreadMXBean;
 import java.util.Enumeration;
 import java.util.Hashtable;
 import java.util.Timer;
@@ -37,29 +40,30 @@ public class TimeThreadHelper extends DefaultMonitoringHelper {
 
 			@Override
 			public void run() {
-				
-					logger.debug("Thread running...");
-					logger.debug("현재 실행중인 쓰레드:" + Thread.currentThread());
-					Enumeration enKey = ht.keys();
-					if (!enKey.hasMoreElements()) {
-						logger.debug("Thread Stop...");
-						timer.cancel();
+
+				logger.debug("Thread running...");
+				logger.debug("현재 실행중인 쓰레드:" + Thread.currentThread());
+				Enumeration enKey = ht.keys();
+				if (!enKey.hasMoreElements()) {
+					logger.debug("Thread Stop...");
+					timer.cancel();
+				}
+
+				while (enKey.hasMoreElements()) {
+					String key = (String) enKey.nextElement();
+					logger.debug("thread key:" + key);
+					long temp = (Long) ht.get(key);
+					long checkTime = System.currentTimeMillis();
+					long longThreadCheck = checkTime - temp;
+					if (longThreadCheck > 3000) {
+						logger.debug("longRunningThread!!    ThreadID:" + key);
+						longCount.put(key, Thread.currentThread());
+						logger.debug("longRunningThread!!    Count:"
+								+ longCount.size());
+						getStackTrace(key);
 					}
-					while (enKey.hasMoreElements()) {
-						String key = (String) enKey.nextElement();
-						logger.debug("thread key:" + key);
-						long temp = (Long) ht.get(key);
-						long checkTime = System.currentTimeMillis();
-						long longThreadCheck = checkTime - temp;
-						if (longThreadCheck > 3000) {
-							logger.debug("longRunningThread!!    ThreadID:"
-									+ key);
-							longCount.put(key, Thread.currentThread());
-							logger.debug("longRunningThread!!    Count:"
-									+ longCount.size());
-						}
-						logger.debug("longThreadCheck:" + longThreadCheck);
-					}
+					logger.debug("longThreadCheck:" + longThreadCheck);
+				}
 			}
 		};
 		timer.scheduleAtFixedRate(timeTask, 0, 1000);
@@ -75,5 +79,18 @@ public class TimeThreadHelper extends DefaultMonitoringHelper {
 
 	public void ip(StringBuffer bf) {
 		logger.debug(bf.toString());
+	}
+
+	public void getStackTrace(String threadID) {
+		long id = Long.parseLong(threadID);
+		int i = 0;
+		ThreadMXBean threadmbean = ManagementFactory.getThreadMXBean();
+		ThreadInfo tr2 = threadmbean.getThreadInfo(id, Integer.MAX_VALUE);
+		StackTraceElement[] ste1 = tr2.getStackTrace();
+		for (StackTraceElement list2 : ste1) {
+			logger.debug("Method_Trace : " + list2.getMethodName() + " (" + i
+					+ ")");
+			i++;
+		}
 	}
 }
