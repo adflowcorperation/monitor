11b8e3de0a284f614ed1a46739aed3de908ae18e
sm_yang <smyang@adflow.co.kr>
2013-06-12 16:05:32 +0900

    src->kr.co.adflow.monitor.system update
    ThreadStackTrace Class

--- adfmonitor/src/kr/co/adflow/monitor/system/ThreadStackTrace.java
+++ adfmonitor/src/kr/co/adflow/monitor/system/ThreadStackTrace.java
@@ -6,42 +6,28 @@ import java.lang.management.ThreadMXBean;
 
 public class ThreadStackTrace {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		Thread test = new Test();
-		test.start();
+	public void getStackTrace(long ltID) {
 
 		ThreadMXBean threadmbean = ManagementFactory.getThreadMXBean();
 
-		System.out.println("Test Class ThreadID : " + test.getId());
-		long threadID = test.getId();
-
-		long ID = threadmbean.getThreadInfo(threadID).getThreadId();
-		System.out.println("ThreadID :" + ID);
-
-		System.out.println("stack lensgth: "
-				+ threadmbean.getThreadInfo(ID, Integer.MAX_VALUE)
-						.getStackTrace().length);
-
-		System.out
-				.println("===============TestClass Thread stack trace test=====================");
-		StackTraceElement[] tr = test.getStackTrace();
-		for (StackTraceElement list : tr) {
-			System.out.println("methodName : " + list.getClassName());
-			System.out.println("className : " + list.getMethodName());
-
-		}
+		// System.out
+		// .println("===============TestClass Thread stack trace test=====================");
+		// StackTraceElement[] tr = test.getStackTrace();
+		// for (StackTraceElement list : tr) {
+		// System.out.println("methodName : " + list.getClassName());
+		// System.out.println("className : " + list.getMethodName());
+		//
+		// }
 
 		System.out
 				.println("=============ThreadStackTraceClass Thread stack trace========================");
-		ThreadInfo tr2 = threadmbean.getThreadInfo(threadID, Integer.MAX_VALUE);
+		ThreadInfo tr2 = threadmbean.getThreadInfo(ltID, Integer.MAX_VALUE);
 
 		StackTraceElement[] ste1 = tr2.getStackTrace();
 		for (StackTraceElement list2 : ste1) {
-			System.out.println("methodName : " + list2.getClassName());
 			System.out.println("className : " + list2.getMethodName());
+			System.out.println("methodName : " + list2.getClassName());
+
 		}
 
 		// System.out
